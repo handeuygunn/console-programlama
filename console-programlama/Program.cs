9c562afc3c9786bc1d53ca754561d65b06fa3166
Hande Nur UYGUN <104138485+handeuygunn@users.noreply.github.com>
2022-06-02 19:26:44 +0300

    Add files via upload

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace console_programlama 
+{
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("İsminizi Giriniz:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Soy İsminizi Giriniz:");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine("Merhaba " + name + " " + surname);
+    }
+}
+}
